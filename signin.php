cb7384fd7c473854e786b6fb26e696cd422fdceb
helenau1 <henzku@hotmail.com>
2017-04-14 13:26:59 +0300

    Sign in page with error handling

--- signin.php
+++ signin.php
@@ -1,52 +1,40 @@
 <?php
-  require_once('dbinfo.php');
+session_start();
+require_once('dbinfo.php');
 
-  // Start a session
-  session_start();
+// Clear the error message
+$message = "";
+// start connection with the database
+$databaseconnection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+// If the user isn't logged in, try to log him/her in
+if (!isset($_SESSION['userId']))
+{
+if(!empty($_POST["sign-in"])) {
 
-  // Clear the error message
-  $error_msg = "";
-
-  // If the user isn't logged in, try to log him/her in
-  if (!isset($_SESSION['userId'])) 
-  {
-    if (isset($_POST['sign-in'])) {
-      // Connect to the database
-      $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-      // Getting the data the user has entered
-      $email = mysqli_real_escape_string($dbc, trim($_POST['email']));
-      $password = mysqli_real_escape_string($dbc, trim($_POST['password']));
-
-      if (!empty($email) && !empty($password)) {
-        // Getting the userId from the database
-        $query = "SELECT userId FROM user WHERE email = '$email' AND password = SHA('$password')";
-        $data = mysqli_query($dbc, $query);
-
-        if (mysqli_num_rows($data) == 1) {
-          // Setting the userid session variable. Set the cookies. Redirect to the home page
-          $row = mysqli_fetch_array($data);
-          
-          $_SESSION['userId'] = $row['userId'];
-         
-          
-          setcookie('userId', $row['userId'], time() + (60 * 60 * 24 * 30));    // expires in 30 days
-          
-          // Getting the URL of the app's home page
-          $home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/index.php';
-          header('Location: ' . $home_url);
-        }
-        else {
-          // The username or password are incorrect so set an error message
-          $error_msg = 'Sorry, the email address or password you entered is not correct. Try again!';
-        }
-      }
-      else {
-        // The username/password weren't entered so set an error message
-        $error_msg = 'Something is missing! Enter your email address and password to sign in.';
-      }
-    }
-  }
+	// Getting the data the user has entered
+	$email = mysqli_real_escape_string($databaseconnection, trim($_POST['email']));
+	$password = mysqli_real_escape_string($databaseconnection, trim($_POST['password']));
+	// Getting the userId from the database
+	$query = "SELECT userId, password FROM user WHERE email = '$email'";
+	$data = mysqli_query($databaseconnection, $query);
+	if (mysqli_num_rows($data) == 1) {
+		// Setting the userid session variable. Set the cookies. Redirect to the home page
+		$row = mysqli_fetch_array($data);
+		//checking the password
+		password_verify($password==$row['password']);
+		
+	
+		$_SESSION['userId'] = $row['userId'];
+		setcookie('userId', $row['userId'], time() + (60 * 60 * 24 * 30));    // expires in 30 days
+	
+		// Getting the URL of the app's home page
+		$home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/index.php';
+		header('Location: ' . $home_url);
+		}
+	} else {
+	$message = "Invalid Username or Password!";
+	}
+}
 ?>
 
 <!DOCTYPE html>
@@ -91,10 +79,7 @@ It allows you to monitor your sports activities and emergency medication use.
   	<input type="submit" name="sign-in" value="Sign in" class="btn btn-default">
 	</form>
 	
-	<?php
-  // If the session data is empty, show any error message and the log-in form; otherwise confirm the log-in
-  if (empty($_SESSION['userId'])) {
-    echo '<p class="text-danger">' . $error_msg . '</p>'; }?> 
+<div class="error-message"><?php if(isset($message)) { echo $message; } ?></div>
 	</div>
 	<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs"> <!-- styling for different screen sizes-->
 	</div>
