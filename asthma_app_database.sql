3d55c76f7402f9a3db04bb718fd58062f4c7e539
helenau1 <henzku@hotmail.com>
2017-05-03 16:04:21 +0300

    Added missing variables

--- asthma_app_database.sql
+++ asthma_app_database.sql
@@ -8,17 +8,18 @@ GRANT DELETE,INSERT,SELECT,UPDATE ON Asthma_app.* TO 'helenau'@'localhost';
 -- Table structure for table user 
 CREATE TABLE user ( 
   userId int NOT NULL AUTO_INCREMENT, 
-  email varchar(16) NOT NULL, 
-  password varchar(40) NOT NULL,  
+  email varchar(100) NOT NULL, 
+  password varchar(255) NOT NULL,  
   PRIMARY KEY (userId) );
   
 -- Table structure for table sportsEvent  
 CREATE TABLE sportsEvent (
     eventId int NOT NULL AUTO_INCREMENT,
-    userId int,
+    userId int NOT NULL,
+    dateof varchar(10) NOT NULL,
     duration int NOT NULL,
-    intensity varchar(6),
-  feeling varchar(5),
+    intensity varchar(10) NOT NULL,
+  feeling varchar(10) NOT NULL,
   inhales int,
   comment varchar(200),
     PRIMARY KEY (eventId),
